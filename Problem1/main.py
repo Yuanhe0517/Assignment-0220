99cc8f5ae486c4f94ec8b1e9655ece25be96967d
Yuan_He <98652026+Yuanhe0517@users.noreply.github.com>
2025-02-19 11:36:07 +0800

    Add files via upload

---
+++ Problem1/main.py
@@ -0,0 +1,10 @@
+# main.py
+
+def calculate_sum():
+    # 学生需在此编写代码
+    return sum(range(1, 51))
+    pass
+
+if __name__ == "__main__":
+    result = calculate_sum()
+    print(f"1+2+...+50的和是：{result}")
---
+++ Problem2/main.py
@@ -0,0 +1,30 @@
+def calculate_polynomial(n: int, x: int, xi_list: list) -> int:
+    """
+    计算多项式展开式 (x - x₁)(x - x₂)...(x - xn) 的值
+
+    
+    参数:
+    n -- 整数，表示xi的数量
+
+    x -- 整数
+
+    xi_list -- 包含n个整数的列表
+
+    
+    返回:
+    展开式的计算结果
+
+    """
+    result = 1
+
+    # 学生实现区
+    for xi in xi_list:
+        result *= (x - xi) 
+        
+    return result
+
+if __name__ == "__main__":
+    n = int(input())
+    x = int(input())
+    xi_list = list(map(int, input().split()))
+    print(calculate_polynomial(n, x, xi_list))
